839c9fd6d222a2811ca7780fe51cc681cfa1c9d0
Darkelan <mobil@pc-koch.de>
2017-03-16 15:58:47 +0100

    Einstellungen-Activity hinzugefügt, Objektliste erstellt

--- app/src/main/java/de/kumodo/rabbitsmart/EinstellungenActivity.java
+++ app/src/main/java/de/kumodo/rabbitsmart/EinstellungenActivity.java
@@ -4,9 +4,11 @@ package de.kumodo.rabbitsmart;
  * Created by l.schmidt on 16.03.2017.
  */
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 public class EinstellungenActivity extends PreferenceActivity
@@ -15,6 +17,13 @@ public class EinstellungenActivity extends PreferenceActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+        Preference objektlistePref = findPreference(getString(R.string.preference_objektliste_key));
+        objektlistePref.setOnPreferenceChangeListener(this);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String gespeicherteObjektliste = sharedPrefs.getString(objektlistePref.getKey(), "");
+        onPreferenceChange(objektlistePref, gespeicherteObjektliste);
 
         Toast.makeText(this, "Einstellungen-Activity gestartet.", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Zurück mit Back-Button.", Toast.LENGTH_SHORT).show();
@@ -22,6 +31,7 @@ public class EinstellungenActivity extends PreferenceActivity
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object value) {
-        return false;
+        preference.setSummary(value.toString());
+        return true;
     }
 }
