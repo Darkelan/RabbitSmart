ba0364401783cd4293f5f47d7787d10c7c45953b
Darkelan <mobil@pc-koch.de>
2017-03-16 15:43:25 +0100

    Einstellungen-Activity hinzugefügt

--- app/src/main/java/de/kumodo/rabbitsmart/MainActivity.java
+++ app/src/main/java/de/kumodo/rabbitsmart/MainActivity.java
@@ -320,7 +320,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(getApplicationContext(), "Einstellungen gedrückt!", Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), "Einstellungen gedrückt!", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, EinstellungenActivity.class));
             return true;
         }
 
